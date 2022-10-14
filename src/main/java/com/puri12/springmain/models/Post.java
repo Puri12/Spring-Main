a549ea62093b17ae2c8292d55251e8590ffdc378
Puri12 <tkdqja9812@gmail.com>
2022-10-14 16:30:17 +0900

    게시글 데이터베이스 모델 작성

---
+++ src/main/java/com/puri12/springmain/models/Post.java
@@ -0,0 +1,28 @@
+package com.puri12.springmain.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Builder
+public class Post extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+}
