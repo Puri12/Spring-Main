910cb5cf55852733738cf0dc662b522c6e631175
Puri12 <tkdqja9812@gmail.com>
2022-10-14 16:29:54 +0900

    댓글 데이터베이스 모델 작성

---
+++ src/main/java/com/puri12/springmain/models/Comment.java
@@ -0,0 +1,25 @@
+package com.puri12.springmain.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Builder
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+}
